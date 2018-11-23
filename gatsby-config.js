9ec2102051b9afe4f43650cfff65a94995dda4d4
Davide Gheri <davide.gheri@docomodigital.com>
2018-11-23 15:07:33 +0100

    removed unused dependencies, fix plugin manifest

--- gatsby-config.js
+++ gatsby-config.js
@@ -23,11 +23,11 @@ module.exports = {
     {
       resolve: `gatsby-plugin-manifest`,
       options: {
-        name: 'gatsby-starter-default',
-        short_name: 'starter',
+        name: 'Davidegheri-personal-website',
+        short_name: 'davidegheri',
         start_url: '/',
-        background_color: '#663399',
-        theme_color: '#663399',
+        background_color: '#4dc0b5',
+        theme_color: '#4dc0b5',
         display: 'minimal-ui',
         icon: 'src/images/gatsby-icon.png', // This path is relative to the root of the site.
       },
