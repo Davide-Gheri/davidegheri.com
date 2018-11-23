6436b4d85ff6d381d1d34d5e14beca6535e4e958
Davide Gheri <davide.gheri@docomodigital.com>
2018-11-23 12:33:33 +0100

    404 page

--- src/pages/404.tsx
+++ src/pages/404.tsx
@@ -1,21 +1,30 @@
 import React from 'react';
 import Layout from '../components/Layout';
+import { PortfolioBackground, PortfolioWrapper, MainSection } from '../components/Styled';
 import styled from 'styled-components';
 
-const Wrapper = styled.section`
+const NotFoundContent = styled.div`
   display: flex;
   align-items: center;
   justify-content: center;
   flex-direction: column;
-  height: 100vh;
+  text-align: center;
+  height: calc(100vh - 48px - 3rem);
+  width: 100%;
 `;
 
 const NotFoundPage = () => (
-  <Layout>
-    <Wrapper>
-      <h1>NOT FOUND</h1>
-      <p>You just hit a route that doesn&#39;t exist... the sadness.</p>
-    </Wrapper>
+  <Layout headerTransparent={false}>
+    <PortfolioBackground>
+      <PortfolioWrapper>
+        <MainSection>
+          <NotFoundContent>
+            <h1>NOT FOUND</h1>
+            <p>You just hit a route that doesn&#39;t exist... the sadness.</p>
+          </NotFoundContent>
+        </MainSection>
+      </PortfolioWrapper>
+    </PortfolioBackground>
   </Layout>
 );
 
